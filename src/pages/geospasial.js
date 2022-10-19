389e52542e7fc2f8b2e379e35c9c0947246fa3a2
Fahmy Nd <travianerst12@gmail.com>
2022-10-19 13:46:17 +0800

    fix bug footer

--- src/pages/geospasial.js
+++ src/pages/geospasial.js
@@ -6,7 +6,8 @@ function Geospasial() {
         document.title = "Geospasial | Website Profil Desa Senga Selatan";
     }, []);
     return (
-        <iframe title='webgis' src="https://sengaselatan.digitaldesa.id/webgis/index.html" frameBorder="0" style={{ overflow: 'hidden', overflowX: 'hidden', overflowY: 'hidden', height: '100%', width: '100%', position: 'absolute', top: '67px', left: '0px', right: '0px', bottom: '0px' }} />
+        <iframe title='webgis' src="https://sengaselatan.digitaldesa.id/webgis/index.html" frameBorder="0" height='580px' width='100%' />
+        // <iframe title='webgis' src="https://sengaselatan.digitaldesa.id/webgis/index.html" frameBorder="0" style={{ overflow: 'hidden', overflowX: 'hidden', overflowY: 'hidden', height: '100%', width: '100%', position: 'absolute', top: '67px', left: '0px', right: '0px', bottom: '0px' }} />
     )
 }
 
--- src/pages/ppid.js
+++ src/pages/ppid.js
@@ -6,9 +6,10 @@ function Ppid() {
         document.title = "PPID | Website Profil Desa Senga Selatan";
     }, []);
     return (
-        <iframe title='ppid' src="https://profil.digitaldesa.id/sengaselatan-luwu/ppid?plainpage" frameBorder="0"
-            style={{ overflow: 'hidden', overflowX: 'hidden', overflowY: 'hidden', height: '100%', width: '100%', position: 'absolute', top: '0px', left: '0px', right: '0px', bottom: '0px' }}
-        />
+        <iframe title='ppid' src="https://profil.digitaldesa.id/sengaselatan-luwu/ppid?plainpage" frameBorder="0" height='1600px' width='100%' scrolling='no' />
+        // <iframe title='ppid' src="https://profil.digitaldesa.id/sengaselatan-luwu/ppid?plainpage" frameBorder="0"
+        //     style={{ overflow: 'hidden', overflowX: 'hidden', overflowY: 'hidden', height: '100%', width: '100%', position: 'absolute', top: '0px', left: '0px', right: '0px', bottom: '0px' }}
+        // />
     )
 }
 
