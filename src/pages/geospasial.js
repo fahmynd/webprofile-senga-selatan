b46b6a2676828633a50990161c822eeae9cfe14f
Fahmy Nd <travianerst12@gmail.com>
2022-10-19 10:36:24 +0800

    fix bug

--- src/pages/geospasial.js
+++ src/pages/geospasial.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Geospasial() {
     return (
-        <iframe title='webgis' src="https://sengaselatan.digitaldesa.id/webgis" width="100%" height="600px"></iframe>
+        <iframe title='webgis' src="//sengaselatan.digitaldesa.id/webgis" width="100%" height="600px"></iframe>
     )
 }
 
--- src/pages/ppid.js
+++ src/pages/ppid.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Ppid() {
     return (
-        <iframe title='ppid' src="https://profil.digitaldesa.id/sengaselatan-luwu/ppid?plainpage" width="100%" height="700px"></iframe>
+        <iframe title='ppid' src="https://profil.digitaldesa.id/sengaselatan-luwu/ppid?plainpage" scrolling='no' width="100%" height="1600px"></iframe>
     )
 }
 
